27fb5bb84ec7e06eb5bd10ec0f85c9a9e3f817b0
Sam Kumar <samkumar99@gmail.com>
2016-04-26 05:38:32 -0700

    Updated benchmark script

---
+++ benchmark/benchmark.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# USAGE: ./copyAndCompile.sh in fuse directory with first argument 
+# being directory of gdpfs to compile
+
+TIMER='/usr/bin/time -f %e'
+
+if [ -z $1 ]; then
+    echo "USAGE: $0 <test>"
+    echo -e "\tmust be run in root of fuse mounted directory"
+    echo -e "\t<test>: gdpfs, redis, or sqlite"
+    exit
+fi
+
+FILELOC=$(dirname $0)
+if [ FILELOC = "." ]; then
+    FILELOC=$(which $0)
+    FILELOC=$(dirname $FILELOC)
+fi
+
+if [ $1 = "gdpfs" ]; then
+    DIR="gdpfs"
+elif [ $1 = "redis" ]; then
+    DIR="redis-3.2"
+elif [ $1 = "sqlite" ]; then
+    DIR="sqlite-autoconf-3120200"
+else
+    echo "Unknown test $1"
+    exit 1
+fi
+
+FILE=$FILELOC/$DIR.tar.gz
+
+if [ ! -z "`ls gdpfs > /dev/null 2> /dev/null`" ]; then
+    echo "Need to delete gdpfs directory first"
+    exit
+fi
+
+accum=0.0
+function timed() {
+    echo "Executing $1"
+    output=`$TIMER $1 2>&1 >/dev/tty`
+    new=`echo $output | tail -n 1 | rev | cut -d ' ' -f 1 | rev`
+    echo "Took $new seconds"
+    accum=`awk "BEGIN{print $new+$accum}"`
+}
+
+timed "cp -r $FILE ."
+timed "tar zxf $FILE"
+cd $DIR
+timed "make"
+echo "TIME IS: $accum seconds"
